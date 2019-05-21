8227b0976273b7202a19b221ac99b57a0d8b0992
macorn <trungnq.b8182@st.usth.edu.vn>
2019-05-21 15:05:23 +0700

    fix OS8(thread)again

--- 08.practical.work.pthread.c
+++ 08.practical.work.pthread.c
@@ -57,15 +57,15 @@ void *consumer(void *para) {
 }
 
 int main() {
-	pthread_t tid1,tid2;
-	pthread_create(&tid1, NULL, producer, NULL);
-    pthread_create(&tid2, NULL, consumer, NULL);
+	    pthread_t tid1,tid2;
+	    pthread_create(&tid1, NULL, producer, NULL);
+        pthread_create(&tid2, NULL, consumer, NULL);
 
         printf("After produce: First: %d Last %d \n", first, last);
         consume();
         printf("After consume: First: %d Last %d \n", first, last);
 		
-    pthread_join(tid1, NULL);
-	pthread_join(tid2, NULL);
-    return 0;
+        pthread_join(tid1, NULL);
+	    pthread_join(tid2, NULL);
+ return 0;
 }
